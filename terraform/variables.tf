aa6fdc6157144118487326bf7c5d140d53e260eb
Vitaly Markov <v.y.markov@gmail.com>
2020-06-28 17:13:29 +0300

    fix(): Update terraform/variables.tf [skip ci]

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -61,13 +61,8 @@ variable "kubernetes" {
     kubernetes_endpoint = string
     kubernetes_token    = string
     kubernetes_ca_cert  = string
+    k8s_provider        = string
   })
-
-  default = {
-    kubernetes_endpoint = ""
-    kubernetes_token    = ""
-    kubernetes_ca_cert  = ""
-  }
 
   description = "Kubernetes credentials which will be used by Kubernetes and Helm providers"
 }
